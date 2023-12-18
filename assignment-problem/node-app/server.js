e6bb4bf95197c9916c67ec3b7eb2e49d28984a2a
Rushabh Shah <rushabh.shah@powerschool.com>
2023-12-18 17:21:47 +0530

    added copy and logs commands

--- assignment-problem/node-app/server.js
+++ assignment-problem/node-app/server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log('Hello from inside the server!');
   res.send(`
     <h1>Hello from inside the very basic Node app!</h1>
   `);
 })
 
+console.log('Server is listening on port 3000...');
 app.listen(3000);
